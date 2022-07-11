cdc7afd9952ffa74248a5725f54265d63ab0a9dc
Trần Văn Phúc <58056634+tranphuc8a@users.noreply.github.com>
2022-07-12 00:13:01 +0700

    Add files via upload

--- BKSNet/index.php
+++ BKSNet/index.php
@@ -1,5 +1,25 @@
 <?php 
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(dirname(__FILE__)) . '/BKSNet');
+
+$controller = null;
+$action = null;
+$params = null;
 	
-	header("location: /BKSNet/apps/controller/loginController.php");
+$url = $_GET['url'] ?? null;
+
+if ($url && $url != ''){
+	include ROOT . '/lib/route.php';
+} else {
+	include ROOT . '/apps/view/layout/loginHeader.php';
+
+	echo 'BKSNet xin chào ' . 
+		 ' <br> Kiểm tra lại địa chỉ url theo định dạng: /BKSNet/controller/action/params...' . 
+		 ' <br> Cảm ơn';
+
+	include ROOT . '/apps/view/layout/loginFooter.php';
+}
+
 
 ?>
