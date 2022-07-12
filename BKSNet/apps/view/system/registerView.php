592e31e46f6341cfd052ac199d429fb470dc99fb
TrinhDucTiep <69502422+TrinhDucTiep@users.noreply.github.com>
2022-07-12 23:09:43 +0700

    Update registerView.php

---
+++ BKSNet/apps/view/system/registerView.php
@@ -0,0 +1,230 @@
+
+
+<style type="text/css">
+	.parent-frame{
+		width: 100%;
+		margin: 0;
+		display: flex;
+		justify-content: center;
+	}
+	.sub-frame{
+		width: 85%;
+		background-color: white;
+		margin: 48px 0px;
+		display: flex;
+		justify-content: center;
+		border-radius: 16px;
+	}
+
+	.register-frame{
+		width: 50%;
+		background-color: #c0fcf6;
+		margin: 48px 0px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border-radius: 16px;
+		flex-direction: column;
+	}
+	.register-frame > *{
+		margin: 24px 0px 0px 0px;
+		width: 85%;
+	}
+	/* input acc and pass */
+	.input-frame{
+		
+	}
+	.input{
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 4px 0px;
+	}
+	.input *{
+		display: inline-block;
+	}
+	.input p{
+		width: 30%;
+	}
+	.input input{
+		width: 70%;
+		border-radius: 4px;
+		height: 28px;
+	}
+	.input input:focus{
+		background-color: lightgray;
+	}
+	/*.input.password input{
+		font-family: sans-serif;
+		font-size: 20px;
+	}*/
+
+	/* input code */
+	.input-code{
+		margin: 0px 0px 24px 0px;
+	}
+	.input-code > *{
+		width: 100%;
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+	}
+	.code-frame p{
+		width: 30%;
+	}
+	.code-frame .code{
+		width: 35%;
+		height: 35px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		margin: 8px 0px;
+		/*border: solid black 1px;*/
+		border-radius: 8px;
+		background-color: gray;
+	}
+	.code-frame .reload{
+		width: 35%;
+		display: flex;
+		justify-content: center;
+	}
+	.reload img{
+		height: 35px;
+		cursor: pointer;
+	}
+
+	.submit-button{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin: 0px 0px 24px 0px;
+	}
+	.submit-button .dangky{
+		width: max(40%, 120px);
+		border: none;
+		border-radius: 8px;
+		background-color: #b00418;
+		color: white;
+		font-family: sans-serif;
+		font-size: 20px;
+		cursor: pointer;
+		padding: 12px;
+	}
+
+	.links{
+		width: 100%;
+		display: flex;
+		justify-content: flex-start;
+		margin: 0;
+	}
+	.links > *{
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin: 0px 0px 24px 0px;
+		height: auto;
+	}
+	.links *{
+		text-decoration: none;
+	}
+</style>
+<script type="text/javascript">
+	function inputPassKeyUp(input){
+		if (input.value.length == 0){
+			input.style = "font-size: 14px";	
+		} else {
+			input.style = "font-size: 24px"; 
+		}
+	}
+	function inputPassKeyDown(input){
+		if (input.value.length == 0){
+			input.style = "font-size: 14px";	
+		} else {
+			input.style = "font-size: 24px"; 
+		}
+	}
+	let randomCode = "";
+	function refreshCode(){
+		let y = "";
+		for (let i = 1; i <= 6; i++){
+			let x = Math.floor(Math.random() * 10);
+			y = y + x.toString();
+		}
+		let code = document.getElementById("code");
+		code.innerHTML = y;
+		randomCode = y;
+	}
+	function checkCode(){
+		input = document.getElementById("input-code");
+		password = document.getElementById("password");
+		repassword = document.getElementById("repassword");
+		account = document.getElementById("account");
+		if(account.value == ""){
+			alert("Vui lòng nhập tên tài khoản");
+
+		} else if(password.value == ""){
+			alert("Bạn chưa nhập mật khẩu");
+		} else if(password.value != repassword.value){
+			alert("Mật khẩu nhập lại không khớp");
+		} else if (randomCode != input.value){
+			alert("Nhập sai mã");
+		}
+		input.value = "";
+		refreshCode();
+	}
+</script>
+
+
+<div class="parent-frame">
+	<div class="sub-frame">
+		<div class="register-frame">
+			<!-- Input account and password -->
+			<div class="input-frame">
+				<div class="input account">
+					<p> Tài khoản </p>
+					<input id="account" type="text" name="account" placeholder="Tên tài khoản" required>
+				</div>
+				<div class="input password">
+					<p> Mật khẩu </p>
+					<input id="password" type="password" name="password" placeholder="Mật khẩu" required onkeyup="inputPassKeyUp(this)">
+				</div>
+				<div class="input password">
+					<p></p>
+					<input id="repassword" type="password" name="password" placeholder="Nhập lại mật khẩu" required onkeyup="inputPassKeyUp(this)">
+				</div>
+			</div>
+
+			<!-- Input code -->
+			<div class="input-code">
+				<div class="code-frame">
+					<p> </p>
+					<div class="code" id="code"> rQstUv </div>
+					<script type="text/javascript">
+						refreshCode();
+					</script>
+					<div class="reload"> 
+						<img src="/BKSNet/img/reload.png" onclick="refreshCode();">
+					</div>
+				</div>
+				<div class="input code">
+					<p> Mã code </p>
+					<input id = "input-code" type="text" name="code" placeholder="Mã xác nhận" required>
+				</div>
+			</div>
+
+			<!-- Submit -->
+			<div class="submit-button">
+				<input class="dangky" type="submit" name="submit" value="Đăng ký" onclick="checkCode()">
+			</div>
+
+			<!-- Forgot pass and sign up -->
+			<div class="links">
+				<div class="center">
+					<a rel="stylesheet" type="text/css" href="/BKSNet/logincontroller/login"> Đăng nhập </a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
