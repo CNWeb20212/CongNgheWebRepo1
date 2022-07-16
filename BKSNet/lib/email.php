aadfe18470c01b70eefcd9795e35d0a57d613dfb
Trần Văn Phúc <58056634+tranphuc8a@users.noreply.github.com>
2022-07-17 02:05:54 +0700

    Tính năng gửi email
    1. Thêm thư viện hỗ trợ gửi email: folder PHPMailer-master
    2. Code lớp email chứa function send giúp gửi email (3 tham số: địa chỉ email nhận, tiêu đề, nội dung)

---
+++ BKSNet/lib/email.php
@@ -0,0 +1,54 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+
+class email{
+    public $mail;
+    public $username, $password;
+
+    public function __construct($username = "bksnet20222@gmail.com", $password = "npygeryczxwrwchn"){
+        $this->username = $username;
+        $this->password = $password;
+
+        $this->mail = new PHPMailer();
+        $this->mail->IsSMTP();
+
+        $this->mail->SMTPDebug  = 0;  
+        $this->mail->SMTPAuth   = TRUE;
+        $this->mail->SMTPSecure = "tls";
+        $this->mail->Port       = 587;
+        $this->mail->Host       = "smtp.gmail.com";
+        // $this->mail->Host       = "smtp.mail.yahoo.com";
+        $this->mail->Username   = $this->username;
+        $this->mail->Password   = $this->password;    
+    }
+
+    public function send($receiver, $subject, $body){
+
+        $this->mail->IsHTML(true);
+        $this->mail->AddAddress($receiver, "receiver");
+        $this->mail->SetFrom($this->username, "BKSNet");
+        //$this->mail->AddReplyTo("reply-to-email", "reply-to-name");
+        //$this->mail->AddCC("cc-recipient-email", "cc-recipient-name");
+
+        $this->mail->Subject = $subject;
+        $content = $body;
+
+        $this->mail->MsgHTML($content); 
+        if(!$this->mail->Send()) {
+            echo "Error while sending Email.";
+            // var_dump($this->mail);
+            return false;
+        } else {
+            echo "Email sent successfully";
+            return true;
+        }
+    }
+
+}
+
+?>
