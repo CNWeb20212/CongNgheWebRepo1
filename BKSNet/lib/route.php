25d39c6ad2485fa1c73ed000f95fe716bd0563d1
TrinhDucTiep <69502422+TrinhDucTiep@users.noreply.github.com>
2022-07-18 00:47:32 +0700

    Update route.php
    thêm hàm gettime

--- BKSNet/lib/route.php
+++ BKSNet/lib/route.php
@@ -66,6 +66,13 @@ function autoloader($className) {
 	}
 }
 
+// hàm gettime theo định dạng y/m/d h/m/s
+function gettime(){
+	$timestamp = time() + 5*3600;
+	$time = date("Y/m/d h:i:s", $timestamp);
+	return $time;
+}
+
 spl_autoload_register('autoloader');
 
 removeMagicQuotes();
