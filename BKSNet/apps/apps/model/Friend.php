b002d39b171a28241004166cbe55945ae9fa96eb
TrinhDucTiep <69502422+TrinhDucTiep@users.noreply.github.com>
2022-07-21 10:13:08 +0700

    sửa lỗi requestfriend
    sửa hàm xóa lời mời kết bạn của chính bản thân

---
+++ BKSNet/apps/apps/model/Friend.php
@@ -0,0 +1,199 @@
+<?php
+class friend extends database{
+	public $ttk1, $ttk2, $datetime;
+
+	public function selectAll($ttk){
+		// check order by
+		$query = "select ttk, ho, dem, ten, school, major from user, profile, friend  where friend.ttk1 = '$ttk' and user.ttk=friend.ttk2 and user.ttk=profile.mssv order by friend.time";
+		try{
+			$table = $this->query($query);
+			$arr = array();
+			while ($row = $table->fetch_assoc()){
+				$item = array(
+					'ttk'			=>$row['ttk'],
+					'hoten'			=>$row['ho'].' '.$row['dem'].' '.$row['ten'],
+					'school'        =>$row['school'],
+					'major'         =>$row['major']
+				);
+				array_push($arr, $item);
+			}
+			return $arr;
+		} catch(Exception $e){
+			echo $e->getMessage();
+		}
+	}
+
+
+	public function selectAllFriendRequest($ttk){
+		$query = "select friendrequest.sender as ttk, ho, dem, ten, school, major from user, profile, friendrequest where user.ttk=friendrequest.sender and friendrequest.sender=profile.mssv and friendrequest.receiver='$ttk' order by friendrequest.time";
+		try{
+			$table = $this->query($query);
+			$arr = array();
+			while($row = $table->fetch_assoc()){
+				$item = array(
+				'ttk'              =>$row['ttk'],
+				'hoten'            =>$row['ho'].' '.$row['dem'].' '.$row['ten'],
+				'school'           =>$row['school'],
+				'major'            =>$row['major']
+				);
+				array_push($arr, $item);
+			}
+			return $arr;
+		} catch(Exception  $e){
+			echo $e->getMessage();
+		}
+	}
+
+	public function selectAllFriendRequested($ttk){
+		$query = "select friendrequest.receiver as ttk, ho, dem, ten, school, major from user, profile, friendrequest where user.ttk=friendrequest.receiver and friendrequest.receiver=profile.mssv and friendrequest.sender='$ttk' order by friendrequest.time";
+		try{
+			$table = $this->query($query);
+			$arr = array();
+			while($row = $table->fetch_assoc()){
+				$item = array(
+				'ttk'              =>$row['ttk'],
+				'hoten'            =>$row['ho'].' '.$row['dem'].' '.$row['ten'],
+				'school'           =>$row['school'],
+				'major'            =>$row['major']
+				);
+				array_push($arr, $item);
+			}
+			return $arr;
+		} catch(Exception  $e){
+			echo $e->getMessage();
+		}
+	}
+
+	public function selectfriend($ttk){
+		$query = "select mssv, email, gender, dateofbirth, address, decription, grade, school, major, ho, dem, ten from profile, user where profile.mssv = '$ttk' and user.ttk=profile.mssv";
+		// $query = "select * from profile right join user on profile.mssv = user.ttk where user.ttk = '$ttk'";
+		try{
+			$table = $this->query($query);
+			$arr = array();
+			while ($row = $table->fetch_assoc()){
+				$item = array(
+					'ho'            =>$row['ho'],
+					'dem'           =>$row['dem'],
+					'ten'           =>$row['ten'],
+					'mssv'			=>$row['mssv'],
+					'email'			=>$row['email'],
+					'gender'		=>$row['gender'],
+					'dateofbirth'	=>substr(strval($row['dateofbirth']), 0, strpos($row['dateofbirth'], ' ')),
+					'address'		=>$row['address'],
+					'decription'	=>$row['decription'],
+					'grade'			=>$row['grade'],
+					'school'		=>$row['school'],
+					'major'			=>$row['major']
+				);
+				array_push($arr, $item);
+			}
+			return isset($arr[0]) ? $arr[0] : null;
+		} catch (Exception $e){
+			echo $e->getMessage();
+		}	
+	}
+
+	public function deletefriend($ttk1, $ttk2){
+		$query = "delete from friend where (friend.ttk1='$ttk1' and friend.ttk2='$ttk2') or (friend.ttk1='$ttk2' and friend.ttk2='$ttk1')";
+		try{
+			$res = $this->query($query);
+			if($res){
+				return true;
+			} else{
+				return false;
+			}
+		} catch(Exception $e){
+			echo $e->getMessage();
+		}
+	}
+
+	public function insertfriend($ttk1, $ttk2){
+		//hàm getttime được chuyển vào trong lib/route
+		$time = gettime();
+		$query = "insert into friend values('$ttk1', '$ttk2', '$time'), ('$ttk2', '$ttk1', '$time')";
+		try{
+			$res = $this->query($query);
+			if($res){
+				return true;
+			} else{
+				return false;
+			}
+		} catch(Exception $e){
+			echo $e->getMessage();
+		}
+	}
+
+	public function insertrequest($receiver){
+		$time = gettime();
+		$sender = $_COOKIE['ttk'];
+		if($_COOKIE['ttk'] == $receiver){
+			return false;
+		}
+		$query = "insert into friendrequest(sender, receiver, time) values('$sender', '$receiver', '$time')";
+		try{
+			$res = $this->query($query);
+			if($res){
+				return true;
+			} else{
+				return false;
+			}
+		} catch(Exception $e){
+			echo $e->getMessage();
+		}
+	}
+
+	public function deleterequest($sender){
+		$receiver = $_COOKIE['ttk'];
+		$query = "delete from friendrequest where sender='$sender' and receiver='$receiver'";
+		try{
+			$res = $this->query($query);
+			if($res){
+				return true;
+			} else{
+				return false;
+			}
+		} catch(Exception $e){
+			echo $e->getMessage();
+		}
+	}
+
+	public function deletemyrequest($receiver){
+		$sender = $_COOKIE['ttk'];
+		$query = "delete from friendrequest where sender=$sender and receiver=$receiver";
+		try{
+			$res = $this->query($query);
+			if($res){
+				return true;
+			} else{
+				return false;
+			}
+		} catch(Exception $e){
+			echo $e->getMessage();
+		}
+	}
+
+
+	//hàm search user
+	public function searchuser($input){
+		$query = "select ttk, concat(ho,' ', dem,' ', ten) as hoten, school, major from profile, user where ((mssv like '%$input%') or (concat(ho,' ', dem,' ', ten) like '%$input%')) and profile.mssv=user.ttk order by mssv";
+		try{
+			$table = $this->query($query);
+			$arr = array();
+			while ($row = $table->fetch_assoc()){
+				$item = array(
+					'ttk'			=>$row['ttk'],
+					'hoten'			=>$row['hoten'],
+					'school'        =>$row['school'],
+					'major'         =>$row['major']
+				);
+				array_push($arr, $item);
+			}
+			return $arr;
+		} catch(Exception $e){
+			echo $e->getMessage();
+		}
+	}
+
+
+}
+?>
---
+++ BKSNet/apps/apps/view/socialnetwork/friend/requestedUserProfileView.php
@@ -0,0 +1,185 @@
+
+<style>
+	.profile-frame{
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.separate{
+		width: 80%;
+		height: 1px;
+		box-sizing: border-box;
+		border: 3px solid #FF9900;
+		border-radius: 8px;
+	}
+	.title-frame{
+		width: 100%;
+		height: 50px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		color: #8c1515;
+		font-weight: bold;
+		font-size: 18px;
+		margin: 64px ;
+	}
+	.content-frame{
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		width: 100%;
+		margin: 32px 0 16px 0;
+		flex-wrap: wrap;
+	}
+	.column{
+		box-sizing: border-box;
+		margin: 8px 16px;
+	}
+	.column-avatar-frame{
+		width: 250px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.column-avatar-frame .avatar{
+		width: 100%;
+		box-sizing: border-box;
+		border-radius: 16px;
+	}
+	.column.c1{
+		width: 400px;
+	}
+	.column.c2{
+		width: 400px;
+	}
+	.item span{
+		line-height: 24px;
+	}
+	.item .title{
+		font-weight: bold;
+	}
+
+	.button-frame{
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.button-frame .button{
+		margin: 64px;
+		margin-bottom: 20px;
+		height: 50px;
+		width: 300px;
+		border-radius: 16px;
+		box-sizing: border-box;
+		font-weight: bold;
+		color: white;
+		background-color: #b00418;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.turn-back{
+		margin-top: 0px;
+		margin-bottom: 10px;
+		padding: 10px;
+		border-radius: 8px;
+		color: red;
+		background-color: #f5edeb;
+	}
+</style>
+
+
+<div class="profile-frame"> 
+	<div class="title-frame">
+		<p>BẠN ĐÃ GỬI LỜI MỜI KẾT BẠN CHO NGƯỜI DÙNG NÀY</p>
+	</div>
+	
+	<div class="separate"></div>
+		
+	<div class="content-frame">
+		<div class="column column-avatar-frame">
+			<img src="/BKSNet/img/avatar.jpg" alt="avatar" class="avatar">
+		</div>
+		<div class="column c1">
+			<p class="item name">
+				<span class="title">Họ và tên:</span>
+				<span class="content">
+					<?php echo $currentuser['ho'] . ' ' . $currentuser['dem'] . ' ' . $currentuser['ten'] ?>		
+				</span>
+			</p>
+			<p class="item">
+				<span class="title">Giới tính:</span>
+				<span class="content">
+					<?php echo $currentuser['gender'] ?>		
+				</span>
+			</p>
+			<p class="item">
+				<span class="title">Ngày sinh:</span>
+				<span class="content">
+					<?php echo $currentuser['dateofbirth']; ?>		
+				</span>
+			</p>
+			<p class="item">
+				<span class="title">Địa chỉ:</span>
+				<span class="content">
+					<?php echo $currentuser['address'] ?>		
+				</span>
+			</p>
+			<p class="item">
+				<span class="title">Mô tả bản thân:</span>
+				<span class="content">
+					<?php echo $currentuser['decription'] ?>		
+				</span>
+			</p>
+		</div>
+		<div class="column c2">
+			<p class="item">
+				<span class="title">MSSV:</span>
+				<span class="content">
+					<?php echo $currentuser['mssv'] ?>		
+				</span>
+			</p>
+			<p class="item">
+				<span class="title">Email:</span>
+				<span class="content">
+					<?php echo $currentuser['email'] ?>		
+				</span>
+			</p>
+			<p class="item">
+				<span class="title">Khóa:</span>
+				<span class="content">
+					<?php echo $currentuser['grade'] ?>		
+				</span>
+			</p>
+			<p class="item">
+				<span class="title">Khoa/Viện/Trường:</span>
+				<span class="content">
+					<?php echo $currentuser['school'] ?>		
+				</span>
+			</p>
+			<p class="item">
+				<span class="title">Chuyên ngành:</span>
+				<span class="content">
+					<?php echo $currentuser['major'] ?>		
+				</span>
+			</p>
+		</div>
+	</div>
+
+	<div class="separate"></div>
+	
+	<div class="button-frame">
+		<form action="/BKSNet/friendcontroller/deletemyrequest/<?php echo $currentuser['mssv'] ?>">
+			<input class="button" type="submit" name="add-button" value="Hủy lời mời kết bạn">
+		</form>
+	</div>
+
+	<!-- turn back -->
+	<div class="button-frame">
+		<a class="turn-back" href="/BKSNet/friendcontroller/listfriend/1">
+			Quay lại
+		</a>
+	</div>
+</div>
